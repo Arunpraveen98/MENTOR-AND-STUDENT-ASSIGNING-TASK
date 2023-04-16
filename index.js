7946540ee471fde7054b4b5f704e049763efc687
Arunpraveen98 <arunfbfb78@gmail.com>
2023-04-16 20:43:03 +0530

    output_added

--- index.js
+++ index.js
@@ -11,6 +11,15 @@ MongoDbConnection();
 // --------------
 //? Middleware to parse request body as JSON
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
+});
 // --------------
 //? Use the mentor and Student routes
 app.use("/Mentor", MentorRouter);
